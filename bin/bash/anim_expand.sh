5b1798ef87bf028597ef593a8fb81f30c7aacddd
ethanwilkins <ethanforrestwilkins@gmail.com>
2019-09-01 10:05:05 -0400

    anim_expand_title_var

--- bin/bash/anim_expand.sh
+++ bin/bash/anim_expand.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-for (( frame_num=41; frame_num<=81; frame_num+=40 ))
+for (( frame_num=41; frame_num<=561; frame_num+=0 ))
 do
+  title='birds_eye'
   folder_num=$((frame_num / 40))
 
-  cp -R birds_eye "birds_eye_$folder_num"
-  cd "birds_eye_$folder_num"
+  cp -R "$title" "${title}_${folder_num}"
+  cd "${title}_${folder_num}"
 
   for frame in *.png; do
-    new=$(printf "birds_eye%04d.png" "$frame_num")
+    new=$(printf "$title%04d.png" "$frame_num")
     mv -i -- "$frame" "$new"
     let frame_num=frame_num+1
   done
 
   cd ..
 done
+
+mv **/*.png "$title/"
